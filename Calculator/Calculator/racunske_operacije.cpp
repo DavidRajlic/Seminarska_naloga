49ce94af742f046ca58e1e1c247d4cf30e70b715
DavidRajlic <55549698+DavidRajlic@users.noreply.github.com>
2023-01-15 01:56:32 +0100

    completed enacba, brez bugov ali pomanjkljivosti (upam)

--- Calculator/Calculator/racunske_operacije.cpp
+++ Calculator/Calculator/racunske_operacije.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <sstream>
 #include <cmath>
+#include <stack>
 using namespace std;
 
 int sestevanje();
@@ -9,25 +10,199 @@ int odstevanje();
 int mnozenje();
 double deljenje();
 int izracun_ostanka();
-int potenciranje();
-int korenjenje();
+int potenciranje(double a, double b);
+double korenjenje(double a);
 double calculate_equation(string equation);
+double enacba(const string& expr) {
+    stack<double> nums;
+    stack<char> ops;
+	 double korenjenec = 0.0;
+    for (int i = 0; i < expr.size(); i++) {
+        char c = expr[i];
+        
+        // ignoriraj presledke 
+        if(c == ' ' || c == ',') continue;
+				// če je število 
+        if (c >= '0' && c <= '9') {
+            string num;
+            while (i < expr.size() && (expr[i] >= '0' && expr[i] <= '9'))
+							num += expr[i++];
+								// pretvori num v double in ga da na vrh stacka
+							nums.push(stod(num));
+							i--;
+        }
+					else if (expr[i] == 's' && expr[i+1] == 'q' && expr[i+2] == 'r' && expr[i+3] == 't'&& expr[i+4] == ' '  ) {
+					i += 5;
+					string num;
+					 string num1;
+					
+					if (expr[i] == '(') {
+						i++;
+					
+							 while (expr[i] != '+' && expr[i] != '-' && expr[i] != '*' && expr[i] != '/' && expr[i] != ')') {
+					 	if (expr[i] >= '0' && expr[i] <= '9') {
+								num += expr[i++];
+								// pretvori num v double in ga da na vrh stacka
+						}
+						else {
+							i++;
+						}
+					}
+				
+				if(expr[i] == '+' || expr[i] == '-' || expr[i] == '*' || expr[i] == '/') {
+					ops.push(expr[i]);
+					i++;
+				}
+				if(expr[i] == ')') {
+					double rezultat = korenjenje(stod(num));
+					nums.push(rezultat);
+				}
+				else {
+	while (expr[i] != ')') {
+						 if ((expr[i] >= '0' && expr[i] <= '9')) {
+								num1 += expr[i++];
+								// pretvori num v double in ga da na vrh stacka
+							nums.push(stod(num1));
+							 }
+							else {
+								i++;
+							}
+					}
+							if (ops.top() == '+') {
+							korenjenec = (stod(num) + stod(num1));
+								double rezultat = korenjenje(korenjenec);
+									nums.top();
+									nums.pop();
+									ops.pop();
+									nums.push(rezultat);
+						}
+						else if (ops.top() == '-')  {
+							korenjenec = (stod(num) - stod(num1));
+								double rezultat = korenjenje(korenjenec);
+									nums.top();
+						nums.pop();
+							ops.pop();
+						nums.push(rezultat);
+						}
+						else if (ops.top() == '*')  {
+							korenjenec = (stod(num) * stod(num1));
+								double rezultat = korenjenje(korenjenec);
+									nums.top();
+						nums.pop();
+							ops.pop();
+						nums.push(rezultat);
+						}
+						else if (ops.top() == '/')  {
+							korenjenec = (stod(num) / stod(num1));
+								double rezultat = korenjenje(korenjenec);
+									nums.top();
+						nums.pop();
+							ops.pop();
+						nums.push(rezultat);
+						}
+				}
+						}
+					 }
+
+					 		else if (expr[i] == 'p' && expr[i+1] == 'o' && expr[i+2] == 'w'  && expr[i+3] == ' ') {
+					i += 4;
+					 string num1;
+					if (expr[i] == '(') {
+						i++;
+					while (expr[i] != ',') {
+						if ((expr[i] >= '0' && expr[i] <= '9')) {
+								num1 += expr[i++];
+								// pretvori num v double in ga da na vrh stacka
+						}
+						else {
+							i++;
+						}
+					}
+							}
+						if(expr[i] == ',') {
+							i++;
+							 string num2;
+						 while (expr[i] != ')') {
+								 if ((expr[i] >= '0' && expr[i] <= '9')) {
+								num2 += expr[i++];
+								// pretvori num v double in ga da na vrh stacka
+							 }
+							else {
+								i++;
+							}
+						
+						}
+						double rezultat = potenciranje(stod(num1), stod(num2));
+						nums.push(rezultat);
+						}
+					}
+				 // če je računska operacija +,-,*,/
+				else if (c == '+' || c == '-' || c == '*' || c == '/') {
+					// dokler stack z operacijami ni prazen in je hkrati na vrhu stacka * ali /
+					
+            while (!ops.empty() && (ops.top() == '*' || ops.top() == '/')) {
+                double num1 = nums.top(); nums.pop();
+                double num2 = nums.top(); nums.pop();
+                char op = ops.top(); ops.pop();
+                if (op == '*') nums.push(num1 * num2);
+                else if (op == '/') nums.push(num2 / num1);
+            }
+            ops.push(c);
+        } else if (c == '(') {
+            ops.push(c);
+        } else if (c == ')') {
+            while (ops.top() != '(') {
+                double num1 = nums.top(); nums.pop();
+                double num2 = nums.top(); nums.pop();
+                char op = ops.top(); ops.pop();
+                if (op == '+') nums.push(num1 + num2);
+                else if (op == '-') nums.push(num2 - num1);
+                else if (op == '*') nums.push(num1 * num2);
+                else if (op == '/') nums.push(num2 / num1);
+            }
+            ops.pop();
+        }
+			
+				
+					
+					// nums.push(koren);
+			
+ 				
+				}
+				 while (!ops.empty()) {
+        double num1 = nums.top(); nums.pop();
+        double num2 = nums.top();
+        char op = ops.top(); ops.pop();
+        if (op == '+') nums.push(num1 + num2);
+        else if (op == '-') nums.push(num2 - num1);
+        else if (op == '*') nums.push(num1 * num2);
+        else if (op == '/') nums.push(num2 / num1);
+    }
+    return nums.top();
+    }
 
 int main () {
 	/*
-		cout << sestevanje() << endl;
+	cout << sestevanje() << endl;
 	cout << odstevanje() << endl;
 	cout << mnozenje() << endl;
 	cout << deljenje() << endl;
 	cout << izracun_ostanka() << endl;
 	cout << potenciranje()  << endl;
 	cout << korenjenje() << endl;
-	*/
-	  string equation;
+	 string equation;
   cout << "Enter an equation: ";
   getline(cin, equation);
   cout << "Result: " << calculate_equation(equation) << endl;
   return 0;
+	
+	*/
+string equation;
+cout << "Vstavi: ";
+
+ getline(cin, equation);
+ int result = enacba(equation);
+ cout << result<< endl;
 	
 }
 
@@ -82,13 +257,7 @@ int izracun_ostanka() {
 	return a - c*b;
 }
 
-int potenciranje() {
-  double a;
-	double b;
-	cout << "Vnesi število: ";
-	cin >> a;
-	cout << "Vnesi število: ";
-	cin >> b;
+int potenciranje(double a, double b) {
 	int rezultat = 1;
 	for (int i = 0; i < b; i++) {
 		rezultat = a*rezultat;
@@ -96,18 +265,12 @@ int potenciranje() {
 	return rezultat;
 }
 
-int korenjenje() {
-	 double a;
-	cout << "Vnesi število: ";
-	cin >> a;
-
+double korenjenje(double a) {
     double rezultat = a / 2;
-
     while ((rezultat * rezultat - a) > 0.001)
     {
         rezultat = (rezultat + a / rezultat) / 2;
     }
-
     return rezultat;
 
 }
@@ -117,14 +280,15 @@ double calculate_equation(string equation) {
   ss << equation;
   double result;
   ss >> result;
-  char op;
-  while (ss >> op) {
+  char operacija;
+  while (ss >> operacija) {
     double term;
     ss >> term;
-    if (op == '+') result += term;
-    else if (op == '-') result -= term;
-    else if (op == '*') result *= term;
-    else if (op == '/') result /= term;
+		if (operacija == '*') result *= term;
+    else if (operacija == '/') result /= term;
+    else if (operacija == '+') result += term;
+    else if (operacija == '-') result -= term;
+  
   }
   return result;
 }
